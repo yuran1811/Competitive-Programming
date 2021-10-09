7c23d9cc37d2ef3e3cdabcdbd9a0730aebbeb773
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-10-09 18:07:06 +0700

    Create CHIADAT.cpp

---
+++ C++/OJ/LQDOJ/CHIADAT.cpp
@@ -0,0 +1,33 @@
+// https://lqdoj.edu.vn/problem/chiadat
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+using namespace std;
+const int N = 505;
+
+int n, a[N][N], f[N][N];
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    	for (int j = 1; j <= n; j++)
+    		cin >> a[i][j];
+
+	for (int i = 1; i <= n; i++)
+    	for (int j = 1; j <= n; j++)
+    		f[i][j] = f[i - 1][j] + f[i][j - 1] - f[i - 1][j - 1] + a[i][j];
+
+    int min_difference = 1e9 + 7;
+	for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+        {
+            int first = f[i][j], second = f[i][n] - first, third = f[n][j] - first, forth = f[n][n] - first - second - third;
+            int	minn = min(second, min(third, min(forth, first)));
+            int maxx = max(second, max(third, max(forth, first)));
+            min_difference = min(maxx - minn, min_difference);
+        }
+	cout << min_difference;
+    return 0;
+}
